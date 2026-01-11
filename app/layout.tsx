12e0aab5b27f0f69e936ddf31b05d31dbebb26bd
myeonghoon <mhoo999@naver.com>
2026-01-11 20:37:31 +0900

    타이틀 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '전화번호 통일',
+  title: '전화번호 형식 통일 서비스',
   description: '엑셀 파일의 전화번호를 통일된 형식으로 변환합니다',
 }
 
